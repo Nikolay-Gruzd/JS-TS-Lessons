78322b7d70bf97c8e506d52ebbba5e6574c7b22e
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-20 22:35:51 +0800

    Lesson 01 - Testing
    Refactoring and Testing functions

--- src/01/01.ts
+++ src/01/01.ts
@@ -1,1 +1,18 @@
-export const value01_01 = '01_01'
+export function sum(a: number, b: number) {
+    return a + b
+}
+
+export function mult(a: number, b: number) {
+    return a * b
+}
+
+export function splitIntoWords(sentense: string) {
+    const words = sentense.toLowerCase().split(' ')
+
+    return words.filter(w => w !== '' && w !== '-')
+        .map(w => w
+            .replace('!', '')
+            .replace('.', '')
+            .replace(',', '')
+        )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import {value01_01} from "./01/01.ts";
 
 export const App = ()=> {
     return (
         <div className="App">
-            {value01_01}
+
         </div>
     );
 }
