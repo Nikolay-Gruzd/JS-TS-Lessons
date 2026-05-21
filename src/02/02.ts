bda1d2262dc5e4dea7919ab1bfb12a5963a51456
Nikolay-Gruzd <gruzd.kolyan@gmail.com>
2026-05-21 21:20:52 +0800

    Lesson 02 - Objects
    Testing objects

---
+++ src/02/02.ts
@@ -0,0 +1,51 @@
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+type CityType = {
+    title: string
+    countryTitle: string
+}
+type TechType = {
+    id: number
+    title: string
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Nikolay',
+    age: 27,
+    isActive: true,
+    address: {
+        streetTitle: 'Lenina',
+        city: {
+            title: 'Irkutsk',
+            countryTitle: 'Russia'
+        },
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML',
+        },
+        {
+            id: 2,
+            title: 'CSS',
+        },
+        {
+            id: 3,
+            title: 'React',
+        },
+    ],
+}
+
+console.log(student.age)
+console.log(student.name)
